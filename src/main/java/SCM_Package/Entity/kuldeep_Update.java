61b3a2c1e215028bd0958dfaf3ce5946c3a33128
kuldeepbanker <kuldeepbanker60@gmail.com>
2025-06-20 17:01:52 +0530

    this is my kuldeepupadate brnach

---
+++ src/main/java/SCM_Package/Entity/kuldeep_Update.java
@@ -0,0 +1,8 @@
+package SCM_Package.Entity;
+
+public class kuldeep_Update
+{
+    public static void main(String[] args) {
+        System.out.println("thin is my new branch in my main project");
+    }
+}
